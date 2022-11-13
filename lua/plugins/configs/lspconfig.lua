45d37a1ee6aeebfee61a2218f3fba0bee295443f
Luca Naef <luca.s.naef@gmail.com>
2022-11-13 10:37:44 -0500

    remote lsp handler

--- lua/plugins/configs/lspconfig.lua
+++ lua/plugins/configs/lspconfig.lua
@@ -15,12 +15,7 @@ local utils = require "core.utils"
 M.on_attach = function(client, bufnr)
   client.server_capabilities.documentFormattingProvider = false
   client.server_capabilities.documentRangeFormattingProvider = false
-
   utils.load_mappings("lspconfig", { buffer = bufnr })
-
-  if client.server_capabilities.signatureHelpProvider then
-    require("nvchad_ui.signature").setup(client)
-  end
 end
 
 M.capabilities = vim.lsp.protocol.make_client_capabilities()
